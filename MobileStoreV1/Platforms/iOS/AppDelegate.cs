9998cdb960efa84c801dac8d3ed4b0e189e6d7f5
DESKTOP-F1RTV5Q\Rami <ramialzend@gmail.com>
2024-05-30 10:42:31 -0700

    first commit

---
+++ MobileStoreV1/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MobileStoreV1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
